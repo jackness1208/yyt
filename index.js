5ef2d36586a92f58183b13d346e22632c5fb10e4
jackNEss <jackness1208@qq.com>
2018-12-06 17:50:33 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const cmd = require('./lib/cmd.js');
+
+const entry = {
+  run: async (ctx, iEnv) => {
+    switch (ctx) {
+      case '-v':
+      case '--version':
+        await cmd.version(iEnv);
+        break;
+
+      case 'start':
+        await cmd.start(iEnv);
+        break;
+
+      case 'abort':
+        await cmd.abort(iEnv);
+        break;
+
+      default:
+        await cmd.help(iEnv);
+        break;
+    }
+  }
+};
+
+module.exports = entry;
